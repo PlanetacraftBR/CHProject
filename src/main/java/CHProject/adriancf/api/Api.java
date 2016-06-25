4f43fa50ec8748a9c28090e75ed11ec1c6d1dbcf
BuildTools <unconfigured@null.spigotmc.org>
2016-06-25 16:33:06 -0300

    Update

---
+++ src/main/java/CHProject/adriancf/api/Api.java
@@ -0,0 +1,51 @@
+/**
+  *           Api.java, Class CH-Group 25 de jun de 2016 - 16:31:46 by αdяiαиcf #Reloads
+  *
+  *###################################################################################
+  *###################################################################################
+  *
+  *101101101101   101101101101   101101101101              10110110101    101101101101
+  *101      101   101            101                       101            101
+  *101      101   101            101                       101            101
+  *101101101101   101            101101101101    101101    101            101
+  *101      101   101            101                       101   101101   101    101101
+  *101      101   101            101                       101      101   101       101
+  *101      101   101101101101   101                       101101101101   101 101101101
+  *
+  *################################################################################### 
+  *###################################################################################
+
+                                   adriancf
+
+  @Projeto CHProject --- proprietário CH-Group, projeto criado no dia 25 de jun de 2016 as 16:31:46        
+   
+  Todo o CHProject.adriancf.api e Api.java, esta sobre os direitos do CH-Group e de suas dependências.                                                     
+  Portantando você, não pode retirar os direitos dos criadores desta class.
+
+
+                                    @Info
+                                   αdяiαиcf
+     
+    *  Dependências utilizadas por CH-Projets  
+    *  Uso de projetos JSon
+    *  Uso de Maven
+    *  Uso de CGlib
+    *  Entre outras...
+
+
+Ass: αdяiαиcf - Códigos livres
+
+
+*/
+
+
+
+package CHProject.adriancf.api;
+
+/**
+ * Código por adriancf, Group CH-Project
+
+ */
+public class Api {
+
+}
