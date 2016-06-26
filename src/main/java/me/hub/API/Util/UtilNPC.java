3797470d12a29c59231fc562453f354031060329
αdяiαиcf <adriancfbr@users.noreply.github.com>
2016-06-26 02:26:40 -0300

    Update NPC
    Retirada do location para entity

--- src/main/java/me/hub/API/Util/UtilNPC.java
+++ src/main/java/me/hub/API/Util/UtilNPC.java
@@ -62,7 +62,7 @@ public class UtilNPC {
 
 	public static HashMap<String,Entity> entitys = new HashMap<String, Entity>();
 	public static HashMap<String,String> nomes = new HashMap<String, String>();
-	public static HashMap<Location,String> location = new HashMap<Location, String>();
+	public static HashMap<Entity,String> location = new HashMap<Entity, String>();
 	public static void SpawnEntity(String nome_entity,String em_baixo,String em_cima,String skin, Location loc)
 	{
 		
@@ -76,11 +76,11 @@ public class UtilNPC {
 		entity.spawn(loc);
         entitys.put(nome_entity,entity.getEntity());
         nomes.put(nome_entity,em_cima);
-        location.put(entity.getEntity().getLocation(), nome_entity);
+        location.put(entity.getEntity(), nome_entity);
 	}
 	
 	/*
-	  UtilNPC.location.get(loc) -- Pegar o nome do NPC no local
+	  UtilNPC.location.get(entity) -- Pegar o nome do NPC no local
 	 
 	
 	*/
@@ -88,7 +88,7 @@ public class UtilNPC {
 	public static void AparecerHolo(Player p)
 	{
 		for (String s : nomes.keySet()) {
-		UtilHolo.showHolo(p,nomes.get(s),entitys.get(s).getLocation().clone().add(0,0,0));
+		UtilHolo.showHolo(p,nomes.get(s),entitys.get(s).getLocation().clone().add(0,-0.5,0));
 		}
 		}
 	
