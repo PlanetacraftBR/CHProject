2daa1dbf4f7bcddeb4d9669ad0137d066dbdd4b5
BuildTools <adriancf@outlook.com.br>
2016-07-15 07:16:26 -0300

    Update Holo

--- src/main/java/me/hub/API/Util/UtilHolo.java
+++ src/main/java/me/hub/API/Util/UtilHolo.java
@@ -156,6 +156,7 @@ public class UtilHolo {
 	    holo.setCustomName(message);
 	    holo.setAI(true);
 	    holo.setNoGravity(true);
+	    loc = loc.clone().add(0,1,0);
 	    holo.setCustomNameVisible(true);
 	    holo.setLocation(loc.getX(), loc.getY(), loc.getZ(), 0, 0);
 	    PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving(holo);
